3243c3c05b9134b2bf98d38d52c0bf1ca8c714ad
thesebonesalone <duikhead2@gmail.com>
2020-11-04 11:55:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+function mapToNegativize(arr) {
+    return arr.map(function(value){
+        return -value
+    })
+}
+
+function mapToNoChange(arr) {
+    return arr
+}
+function mapToDouble(arr) {
+    return arr.map(function(value){
+        return value * 2
+    })
+}
+
+function mapToSquare(arr) {
+    return arr.map(function(value){
+        return value * value
+    })
+}
+
+function reduceToTotal(arr, start = 0) {
+    let total = start
+    for (let i = 0; i < arr.length; i += 1){
+        total += arr[i]
+    }
+    return total
+} 
+
+function reduceToAllTrue(arr) {
+    let flip = true
+    arr.forEach(function(value){
+        if (!value){
+            flip = false
+        }
+    })
+    return flip
+}
+
+function reduceToAnyTrue(arr) {
+    let flip = false
+    arr.forEach(function(value){
+        if (value){
+            flip = true
+        }
+    })
+    return flip
+}
